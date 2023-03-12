3c25f9dcf26e83def8e75891cceccd05f5636aa9
Soneta666 <90927771+Soneta666@users.noreply.github.com>
2023-03-12 21:58:10 +0200

    Update and rename BusinessLogic/Entities/TypeEngine.cs to БізнесЛогіка/Сутності/TypeEngine.cs

--- БізнесЛогіка/Сутності/TypeEngine.cs
+++ БізнесЛогіка/Сутності/TypeEngine.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public ICollection<TypeEngine> Engines { get; set; }
+        public ICollection<Engine> Engines { get; set; }
     }
 }
